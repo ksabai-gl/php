dcd7d25c9ef20033b280abd9290bfa754d5c8607
ksabai-gl <kanagasabai.rangasamy@globallogic.com>
2026-07-20 11:12:26 +0530

    MAD-134: migrate PHP monolith to modular Node.js and Liferay portlets to React SPA - server/src/utils/httpError.js

---
+++ server/src/utils/httpError.js
@@ -0,0 +1,12 @@
+'use strict';
+
+class HttpError extends Error {
+  constructor(status, message, details) {
+    super(message);
+    this.name = 'HttpError';
+    this.status = status;
+    if (details) this.details = details;
+  }
+}
+
+module.exports = { HttpError };
